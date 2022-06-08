aad1cbf6c7dc2b7951b5e154d83e374cb9539ec3
jatin vats <jatinvats525@gmail.com>
2022-06-08 11:08:59 +0530

    +

---
+++ React/classes/inheritance.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor() {
+    this.name = "Jatin Vats";
+    this.age = 23;
+  }
+  printName = () => console.log(this.name, this.age);
+}
+
+const person = new Person();
+person.printName();
