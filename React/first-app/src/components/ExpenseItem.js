bbecb82f5a7bb940016932870911381c31908ad6
jatinvats636 <jatinvats525@gmail.com>
2022-06-09 12:34:48 +0530

    + ExpenseDate.js

--- React/first-app/src/components/ExpenseItem.js
+++ React/first-app/src/components/ExpenseItem.js
@@ -1,12 +1,23 @@
 import "./ExpenseItem.css";
 
-let ExpenseItem = () => {
+let ExpenseItem = (props) => {
+  // const expenseDate = new Date(2022, 5, 9);
+  // const expenseTitle = "New Laptop";
+  // const expenseAmount = 1500;
+  const expenseMonth = props.date.toLocaleString("en-US", { month: "long" });
+  const expenseDay = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const expenseYear = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div></div>
+      <div>
+        <div>{expenseMonth}</div>
+        <div>{expenseYear}</div>
+        <div>{expenseDay}</div>
+      </div>
       <div className="expense-item_description">
-        <h2></h2>
-        <div className="expense-item_price"></div>
+        <h2>{props.title}</h2>
+        <div className="expense-item_price">${props.amount}</div>
       </div>
     </div>
   );
