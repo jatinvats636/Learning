f92ea66b1ff9294aa6b825984d3ede1aca793c4d
jatinvats636 <jatinvats525@gmail.com>
2022-06-13 17:08:54 +0530

    + styling changed

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -8,19 +8,36 @@ function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
+      title: "New Laptop",
+      amount: 120.12,
+      date: new Date(2022, 6, 6),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New Television",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: "e3",
+      title: "Earphones",
+      amount: 10.11,
+      date: new Date(2022, 6, 6),
+    },
+    {
+      id: "e4",
+      title: "T-Shirt",
+      amount: 12.12,
+      date: new Date(2022, 6, 6),
+    },
+    {
+      id: "e5",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: "e4",
+      id: "e6",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
