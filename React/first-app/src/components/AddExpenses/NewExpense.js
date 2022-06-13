6c14feab753208ec51ba66180b9c9869bc203ac5
jatinvats636 <jatinvats525@gmail.com>
2022-06-13 10:48:35 +0530

    + ExpenseForm.js

--- React/first-app/src/components/AddExpenses/NewExpense.js
+++ React/first-app/src/components/AddExpenses/NewExpense.js
@@ -1,3 +1,12 @@
-const NewExpense = () => {};
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
 
 export default NewExpense;
