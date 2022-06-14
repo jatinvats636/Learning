11c82a35feafb1154798f7b472bc813b9e6c3b6a
jatinvats636 <jatinvats525@gmail.com>
2022-06-14 13:15:47 +0530

    + child to parent prop

--- React/first-app/src/components/AddExpenses/NewExpense.js
+++ React/first-app/src/components/AddExpenses/NewExpense.js
@@ -2,9 +2,15 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expeneseData = {
+      ...enteredExpenseData,
+      id: Math.random.toString(),
+    };
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
