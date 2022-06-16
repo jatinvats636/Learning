54efa8a32fd5386842ad5bdea3bcbcaf429c28e9
jatinvats636 <jatinvats525@gmail.com>
2022-06-16 11:42:10 +0530

    + ExpenseFilter.js

--- React/first-app/src/components/FilterExpenses/ExpenseFilter.js
+++ React/first-app/src/components/FilterExpenses/ExpenseFilter.js
@@ -2,8 +2,16 @@ import "./ExpenseFilter.css";
 
 const ExpenseFilter = () => {
   return (
-    <div>
-      <div></div>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
     </div>
   );
 };
