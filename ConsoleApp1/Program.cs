857bc39b3af1061141650b0efd413fe63b30f513
jatinvats636 <jatinvats525@gmail.com>
2022-07-11 16:25:18 +0530

    +

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double numDouble = 1.534567;
+            //int numInt = (int)numDouble;
+            //Console.WriteLine("Original double Value: " + numDouble);
+            //Console.WriteLine("Converted int Value: " + numInt);
+
+            try
+            {
+                Console.Write("Enter a string : ");
+                string s = Console.ReadLine();
+                int i = Convert.ToInt32(s);
+                int j = int.Parse(s);
+                Console.WriteLine("{0},{1}", i, j);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n\nCan't convert !!!");
+            }
+
+            
+        }
+    }
+}
