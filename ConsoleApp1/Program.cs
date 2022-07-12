bbbbd3f81c319d8552077bf896b81d2a875ff3fd
jatinvats636 <jatinvats525@gmail.com>
2022-07-12 14:01:34 +0530

    arrays

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,20 +11,24 @@ namespace MyApplication
             //Console.WriteLine("Original double Value: " + numDouble);
             //Console.WriteLine("Converted int Value: " + numInt);
 
-            try
-            {
-                Console.Write("Enter a string : ");
-                string s = Console.ReadLine();
-                int i = Convert.ToInt32(s);
-                int j = int.Parse(s);
-                Console.WriteLine("{0},{1}", i, j);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("\n\nCan't convert the provided string !!!");
-            }
+            //try
+            //{
+            //    Console.Write("Enter a string : ");
+            //    string s = Console.ReadLine();
+            //    int i = Convert.ToInt32(s);
+            //    int j = int.Parse(s);
+            //    Console.WriteLine("{0},{1}", i, j);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("\n\nCan't convert the provided string !!!");
+            //}
 
-            
+            int[] nums = new int[4];
+            nums[0] = 1;
+            nums[1] = 2;
+            nums[2] = 3;
+            Console.WriteLine(nums[2] + " & " + nums[3]);
         }
     }
 }
